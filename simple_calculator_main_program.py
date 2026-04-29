a3a511b3b4555194f29afccc3ac548ce2549bd92
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-04-29 22:55:54 +0800

    created a method that clears the terminal screen for the readability of calculator

--- simple_calculator_main_program.py
+++ simple_calculator_main_program.py
@@ -8,3 +8,6 @@ from calculator_functionalities import (
 class SimpleCalculator:
     def __init__(self):
         self.clear_screen()
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
