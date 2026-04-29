14e7d528412dca3e4498e963c0c3c35ad8f98d5c
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-04-29 23:13:19 +0800

    created a method that displays the menu like the operations and the history

--- simple_calculator_main_program.py
+++ simple_calculator_main_program.py
@@ -20,3 +20,11 @@ class SimpleCalculator:
 ╚══════════════════════════════════════════════════════╝  
 {Style.RESET_ALL}"""
         print(banner)
+
+    def display_menu(self):
+        print(f"{Fore.CYAN}Please select an operation:{Style.RESET_ALL}")
+        for key, (name, _) in CalculatorActions.operations.items():
+            print(f"{Fore.GREEN}{key}{Style.RESET_ALL}. {name}")
+        print(f" {Fore.YELLOW}H{Style.RESET_ALL}. View History")
+        print(f" {Fore.RED}Q{Style.RESET_ALL}. Quit")
+        print("=" * 50)
