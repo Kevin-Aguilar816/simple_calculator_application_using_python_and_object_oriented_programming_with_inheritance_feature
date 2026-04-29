f709cdfd6fecf1aac9d68b80ee03eee8128eeec4
Kevin-Aguilar816 <aguilarvhonkevinjames@gmail.com>
2026-04-29 22:55:00 +0800

    created the simple calculator class which is the main calculator class

--- simple_calculator_main_program.py
+++ simple_calculator_main_program.py
@@ -3,3 +3,8 @@ import time
 import sys
 from calculator_functionalities import (
     CalculatorActions, DivisionByZeroError, InvalidInputError)
+
+
+class SimpleCalculator:
+    def __init__(self):
+        self.clear_screen()
